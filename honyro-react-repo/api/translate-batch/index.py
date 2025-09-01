bf8d4f698f2e701b4a2029045b93c5d38d96c0b6
AndrsDR <andres.diaz.r.dev@gmail.com>
2025-09-01 14:33:02 -0600

    Update index.py

--- honyro-react-repo/api/translate-batch/index.py
+++ honyro-react-repo/api/translate-batch/index.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from server.main import BatchPayload, translate_batch
-
 app = FastAPI()
 
 @app.post("/")
+@app.post("/api/translate-batch")
+@app.post("/api/translate-batch/")
 def _translate_batch(payload: BatchPayload):
     return translate_batch(payload)
