ce613d10726a099168cf0dc76c6c0453bd561af9
AndrsDR <andres.diaz.r.dev@gmail.com>
2025-09-01 14:32:46 -0600

    Update index.py

--- honyro-react-repo/api/translate/index.py
+++ honyro-react-repo/api/translate/index.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from server.main import TranslatePayload, translate
-
 app = FastAPI()
 
 @app.post("/")
+@app.post("/api/translate")
+@app.post("/api/translate/")
 def _translate(payload: TranslatePayload):
     return translate(payload)
